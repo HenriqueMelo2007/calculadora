57b7ef7756457076e2941da69fa9a1f720d5be1d
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-10-10 18:38:30 -0300

    melhorando o código da função dos botões de número

--- script.js
+++ script.js
@@ -10,48 +10,20 @@ const sete = document.querySelector('#sete')
 const oito = document.querySelector('#oito')
 const nove = document.querySelector('#nove')
 
-
-zero.addEventListener('click', addZero)
-um.addEventListener('click', addUm)
-dois.addEventListener('click', addDois)
-tres.addEventListener('click', addTres)
-quatro.addEventListener('click', addQuatro)
-cinco.addEventListener('click', addCinco)
-seis.addEventListener('click', addSeis)
-sete.addEventListener('click', addSete)
-oito.addEventListener('click', addOito)
-nove.addEventListener('click', addNove)
+zero.addEventListener('click', () => add('1'))
+um.addEventListener('click', () => add('1'))
+dois.addEventListener('click', () => add('2'))
+tres.addEventListener('click', () => add('3'))
+quatro.addEventListener('click', () => add('4'))
+cinco.addEventListener('click', () => add('5'))
+seis.addEventListener('click', () => add('6'))
+sete.addEventListener('click', () => add('7'))
+oito.addEventListener('click', () => add('8'))
+nove.addEventListener('click', () => add('9'))
 
 // Resultado
 const resultado = document.querySelector('#resultado')
 
-function addZero() {
-  resultado.innerHTML += `${zero.innerHTML}`
-}
-function addUm() {
-  resultado.innerHTML += `${um.innerHTML}`
-}
-function addDois() {
-  resultado.innerHTML += `${dois.innerHTML}`
-}
-function addTres() {
-  resultado.innerHTML += `${tres.innerHTML}`
-}
-function addQuatro() {
-  resultado.innerHTML += `${quatro.innerHTML}`
-}
-function addCinco() {
-  resultado.innerHTML += `${cinco.innerHTML}`
-}
-function addSeis() {
-  resultado.innerHTML += `${seis.innerHTML}`
-}
-function addSete() {
-  resultado.innerHTML += `${sete.innerHTML}`
-}
-function addOito() {
-  resultado.innerHTML += `${oito.innerHTML}`
-}
-function addNove() {
-  resultado.innerHTML += `${nove.innerHTML}`
+function add(value) {
+  resultado.innerHTML += value
 }
