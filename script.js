64a3d22f1b380d6971e82eca325db5f44dc713e8
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-10-10 20:51:48 -0300

    melhorando funções dos números e criando função para o ponto que serve para separar casas decimais

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ const sete = document.querySelector('#sete')
 const oito = document.querySelector('#oito')
 const nove = document.querySelector('#nove')
 
-zero.addEventListener('click', () => add('1'))
-um.addEventListener('click', () => add('1'))
-dois.addEventListener('click', () => add('2'))
-tres.addEventListener('click', () => add('3'))
-quatro.addEventListener('click', () => add('4'))
-cinco.addEventListener('click', () => add('5'))
-seis.addEventListener('click', () => add('6'))
-sete.addEventListener('click', () => add('7'))
-oito.addEventListener('click', () => add('8'))
-nove.addEventListener('click', () => add('9'))
+zero.addEventListener('click', () => add(0))
+um.addEventListener('click', () => add(1))
+dois.addEventListener('click', () => add(2))
+tres.addEventListener('click', () => add(3))
+quatro.addEventListener('click', () => add(4))
+cinco.addEventListener('click', () => add(5))
+seis.addEventListener('click', () => add(6))
+sete.addEventListener('click', () => add(7))
+oito.addEventListener('click', () => add(8))
+nove.addEventListener('click', () => add(9))
+
+const ponto = document.querySelector('#ponto')
+
+ponto.addEventListener('click', () => add('.'))
+
+
 
 // Resultado
 const resultado = document.querySelector('#resultado')
